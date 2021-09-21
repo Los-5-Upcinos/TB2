8091173e7a391baced434b44fe082d671d5a39fe
Rodrigo Toconas <u201713849@upc.edu.pe>
2021-09-21 15:14:16 -0500

    daoIMPL

--- src/main/java/daoimpl/ICursoDaoImpl.java
+++ src/main/java/daoimpl/ICursoDaoImpl.java
@@ -2,27 +2,60 @@ package daoimpl;
 
 import dao.ICursoDao;
 import entities.Curso;
-
+import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.*;
+import javax.transaction.Transactional;
 
 public class ICursoDaoImpl implements ICursoDao {
+    @PersistenceContext(unitName = "PrograWeb")
+    private EntityManager entityManager;
+
+    @Transactional
     @Override
     public void insertar(Curso curso) {
+        try
+        {
+            entityManager.persist(curso);
+        }catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("No se puede listar");
+        }
 
     }
-
+    @SuppressWarnings("unchecked")
     @Override
     public List<Curso> listar() {
-        return null;
+        List<Curso> lista = new ArrayList<Curso>();
+        try {
+            Query q;
+            q = entityManager.createQuery("select Curso from Curso s");
+            lista = (List<Curso>) q.getResultList();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return lista;
+
     }
 
     @Override
     public void eliminar(int idCurso) {
-
+        Curso c = new Curso();
+        try {
+            c = entityManager.getReference(Curso.class, idCurso);
+            entityManager.remove(c);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
-
     @Override
     public void modificar(Curso curso) {
-
+        try {
+            entityManager.merge(curso);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
