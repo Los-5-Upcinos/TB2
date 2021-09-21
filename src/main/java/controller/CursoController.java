8c6f3bf90a2d61ddabab59c479f8e944a946e068
Rodrigo Toconas <u201713849@upc.edu.pe>
2021-09-21 16:02:12 -0500

    Curso Controller

---
+++ src/main/java/controller/CursoController.java
@@ -0,0 +1,95 @@
+package controller;
+
+import entities.Curso;
+import dao.ICursoDao;
+import service.ICursoService;
+
+import java.util.*;
+import javax.annotation.PostConstruct;
+import javax.inject.*;
+import javax.enterprise.context.RequestScoped;
+
+@Named
+@RequestScoped
+public class CursoController {
+    @Inject
+    private ICursoService cursoService;
+    private String mensaje = "Error";
+    private Curso curso;
+    List<Curso> cursoList;
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    @PostConstruct
+    public void init() {
+        this.cursoList = new ArrayList<Curso>();
+        this.curso = new Curso();
+        this.listar();
+    }
+
+    public String newCurso() {
+        this.setCurso(new Curso());
+        return "Curso.xhtml";
+    }
+
+    public void insertar() {
+        try {
+
+            cursoService.insertar(curso);
+            this.listar();
+        } catch (Exception e) {
+            e.getMessage();
+        }
+    }
+
+    public void eliminar(Curso curso) {
+        try {
+            cursoService.eliminar(curso.getId());
+            listar();
+        } catch (Exception e) {
+            e.getMessage();
+        }
+    }
+
+    public void listar() {
+        try {
+            cursoList = cursoService.listar();
+        } catch (Exception e) {
+            e.getMessage();
+        }
+    }
+
+    public void modificar() {
+        try {
+            cursoService.modificar(this.curso);
+            this.listar();
+
+        } catch (Exception e) {
+            e.getMessage();
+        }
+    }
+
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+
+    public List<Curso> getCursoList() {
+        return cursoList;
+    }
+
+    public void setCursoList(List<Curso> cursoList) {
+        this.cursoList = cursoList;
+    }
+
+}
