569f1d093f5e0a01364b692d6a85358c7cd4babf
KevinQT <85418848+KevinQT@users.noreply.github.com>
2021-09-28 14:09:09 -0500

    Update ITareaService.java

--- src/main/java/service/ITareaService.java
+++ src/main/java/service/ITareaService.java
@@ -1,17 +1,18 @@
 package service;
+
 import java.util.List;
 
 import entities.Tarea;
+
 public interface ITareaService {
 
     public void insertar(Tarea tarea);
-
-    public void eliminar(int idEstudiante);
-
+    
+    public void eliminar(int id_tareas);
+    
     public List<Tarea> listar();
-
+    
     public void modificar(Tarea tarea);
 
-
-
+   
 }
