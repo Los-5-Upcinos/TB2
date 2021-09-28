aa7912fcb4e5dfb0e487bcab39d6e6d97d8d4358
KevinQT <85418848+KevinQT@users.noreply.github.com>
2021-09-28 14:07:03 -0500

    Update ITareaServiceImpl.java

--- src/main/java/serviceimpl/ITareaServiceImpl.java
+++ src/main/java/serviceimpl/ITareaServiceImpl.java
@@ -1,28 +1,39 @@
 package serviceimpl;
 
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import dao.ITareaDao;
 import entities.Tarea;
 import service.ITareaService;
 
-import java.util.List;
-
+@Named
+@RequestScoped
 public class ITareaServiceImpl implements ITareaService {
+	
+	@Inject
+	private ITareaDao tDao;
+	
     @Override
     public void insertar(Tarea tarea) {
-
+    	tDao.insertar(tarea);
     }
 
     @Override
-    public void eliminar(int idEstudiante) {
-
+    public void eliminar(int id_tareas) {
+    	tDao.eliminar(id_tareas);
     }
-
+    
     @Override
     public List<Tarea> listar() {
-        return null;
+        return tDao.listar();
     }
 
-    @Override
+  
     public void modificar(Tarea tarea) {
-
+    	tDao.modificar(tarea);
     }
 }
