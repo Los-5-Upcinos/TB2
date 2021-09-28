e2cf13b57a8fb42d3854a1f2fc96da9a3dad7d13
KevinQT <85418848+KevinQT@users.noreply.github.com>
2021-09-28 14:13:45 -0500

    Update ITareaDao.java

--- src/main/java/dao/ITareaDao.java
+++ src/main/java/dao/ITareaDao.java
@@ -1,12 +1,17 @@
 package dao;
-import entities.*;
+
 import java.util.List;
+
+import entities.Tarea;
+
 public interface ITareaDao {
     public void insertar(Tarea tarea);
 
     public List<Tarea> listar();
-
-    public void eliminar(int idTarea);
-
+    
+    public void eliminar(int id_tareas);
+    
     public void modificar(Tarea tarea);
+
+
 }
