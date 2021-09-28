3dbcc24662e53dc85fc5c57c8606403a233835a9
KevinQT <85418848+KevinQT@users.noreply.github.com>
2021-09-28 14:06:10 -0500

    Update IEstudianteServiceImpl.java

--- src/main/java/serviceimpl/IEstudianteServiceImpl.java
+++ src/main/java/serviceimpl/IEstudianteServiceImpl.java
@@ -1,28 +1,41 @@
 package serviceimpl;
 
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import dao.IEstudianteDao;
 import entities.Estudiante;
 import service.IEstudianteService;
 
-import java.util.List;
-
+@Named
+@RequestScoped
 public class IEstudianteServiceImpl implements IEstudianteService {
+	
+	@Inject
+	private IEstudianteDao eDao;
+	
     @Override
     public void insertar(Estudiante estudiante) {
-
+    	eDao.insertar(estudiante);
     }
-
+    
     @Override
-    public void eliminar(int idEstudiante) {
-
+    public void eliminar(int id_estudiante) {
+    	eDao.eliminar(id_estudiante);
     }
 
     @Override
     public List<Estudiante> listar() {
-        return null;
+        return eDao.listar();
     }
+    
 
     @Override
-    public void modificar(Estudiante estudiante) {
-
+    public List<Estudiante> findByNameEstudiante(Estudiante estudiante) {
+        return eDao.findByNameEstudiante(estudiante);
     }
+
 }
