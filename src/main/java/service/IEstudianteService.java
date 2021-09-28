1737ba78cbc8cd33ed962ee315a6e1cd70c2ae24
KevinQT <85418848+KevinQT@users.noreply.github.com>
2021-09-28 14:07:36 -0500

    Update IEstudianteService.java

--- src/main/java/service/IEstudianteService.java
+++ src/main/java/service/IEstudianteService.java
@@ -5,11 +5,10 @@ public interface IEstudianteService {
 
     public void insertar(Estudiante estudiante);
 
-    public void eliminar(int idEstudiante);
-
-    public List<Estudiante> listar();
-
-    public void modificar(Estudiante estudiante);
+    public void eliminar(int id_estudiante);
+    List<Estudiante> listar();
+    
+    public List<Estudiante> findByNameEstudiante(Estudiante estudiante);
 
 
 }
